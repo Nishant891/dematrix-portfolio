ed97a50d9e4f99afc6de98225a727c115a71c607
Nishant891 <nishant19072003@gmail.com>
2025-05-13 00:07:13 +0530

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics/>
+        <SpeedInsights/>
+      </body>
     </html>
   )
 }
